600989871bedcba0c0b41689299bd750e77739dc
Abdul Salam <abdulsalam121196@gmail.com>
2023-12-01 19:19:47 +0700

    refactor: product v2

--- app/productV2/controller.js
+++ app/productV2/controller.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 const Product = require('./model');
 const { Op } = require('sequelize');
 
+// Default location of file.
+const defaultPath = '../../uploads/public';
 
 const index = async (req, res) => {
-    try { 
-        const {search} = req.query;
+    try {
+        const { search } = req.query;
         let data = {};
 
         if (search) {
@@ -16,15 +18,14 @@ const index = async (req, res) => {
                         [Op.like]: [`%${search}%`]
                     },
                 },
-            }); 
+            });
         } else {
             data = await Product.findAll();
-        } 
-        
-        res.json(data);
+        }
 
+        res.json(mappingProductData(req, res)(data));
     } catch (e) {
-        console.error(e); 
+        console.error(e);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
@@ -32,27 +33,30 @@ const index = async (req, res) => {
 const view = async (req, res) => {
     try {
         const byuserID = req.params.id;
-        let data = await Product.findAll({ 
-            where: {id: byuserID}
+        let data = await Product.findAll({
+            where: { id: byuserID }
         });
-         
-        res.json(data);
+
+        res.json(mappingProductData(req, res)(data));
     } catch (e) {
         console.error(e);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-}     
+}
 
 const store = async (req, res) => {
-    const {users_id, name, price, stock, status} = req.body;
+    const { users_id, name, price, stock, status } = req.body;
     const image = req.file;
-    if(image) {
-        const target = path.join(__dirname, '../../uploads', image.originalname);
+
+    if (image) {
+        const target = path.join(__dirname, defaultPath, image.originalname);
         fs.renameSync(image.path, target);
     }
+
     try {
         const imgUrl = `/public/${image.originalname}`;
-        const result = await Product.create({users_id, name, price, stock, status, image_url: req.baseUrl + imgUrl });
+        const result = await Product.create({ users_id, name, price, stock, status, image_url: imgUrl });
+
         res.json(result);
     } catch (e) {
         console.error(e);
@@ -62,10 +66,11 @@ const store = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const {users_id, name, price, stock, status} = req.body;
+        const byuserID = req.params.id;
+        const { users_id, name, price, stock, status } = req.body;
         const image = req.file;
 
-        let dataUpdate = {
+        const dataUpdate = {
             users_id: parseInt(users_id),
             name,
             price,
@@ -73,40 +78,57 @@ const update = async (req, res) => {
             status,
         };
 
-        let imageUpdate = {};
         if (image) {
-            const target = path.join(__dirname, '../../uploads', image.originalname);
+            const target = path.join(__dirname, defaultPath, image.originalname);
             fs.renameSync(image.path, target);
-            const imgUrl = `/public/${image.originalname}`;
-            imageUpdate['image_url'] = req.baseUrl + imgUrl;
-        }
-        
-        const byuserID = req.params.id;
 
-        const [ProdukUpdate] = await Product.update(dataUpdate, {
-            where: {id: byuserID},
+            const imgUrl = `/public/${image.originalname}`;
+            dataUpdate['image_url'] = imgUrl;
+        }
+
+        const [ProductUpdate] = await Product.update(dataUpdate, {
+            where: { id: byuserID },
             returning: true,
         });
-        res.json(ProdukUpdate);
-    } catch (e) {
-        console.error(e); 
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
 
-const destroy = async (req, res) => {
-    try{
-        const byuserID = req.params.id;
-        const ProdukDelete = await Product.destroy({
-            where: {id: byuserID},            
-        });
-        res.json(ProdukDelete);
+        res.json(ProductUpdate);
     } catch (e) {
         console.error(e);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
+const destroy = async (req, res) => {
+    try {
+        const byuserID = req.params.id;
+        const ProductDelete = await Product.destroy({
+            where: { id: byuserID },
+        });
+
+        // TODO:
+        // Mising delete file from uploads/public one.
+
+        res.json(ProductDelete);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+/**
+ * Helper function.
+ * 1. Get the full url without any intercept.
+ * 2. General Mappers products data.
+ */
+const fullHostUrl = (req, res) => (req.protocol + '://' + req.get('host'));
+const mappingProductData = (req, res) => (data) => {
+    const mProduct = data.map((product) => {
+        product.image_url = fullHostUrl(req, res) + product.image_url
+        return product;
+    });
+
+    return mProduct;
+}
 
 module.exports = {
     index,
